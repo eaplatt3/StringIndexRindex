6c4da0230e6b6e80071ad1cefc75dd7141f5bad6
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-03-07 12:06:40 -0500

    Initial commit

---
+++ StringIndexRindex.pl
@@ -0,0 +1,10 @@
+####################################
+# Earl Platt III
+# 3/7/2019
+# BCS 316 - Perl Programming
+# String Index & Rindex
+# Input - User Enters Long String
+#         & Short String
+# Output - Comparsion of the two
+#          Strings
+####################################
